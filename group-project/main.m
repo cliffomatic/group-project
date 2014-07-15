a6f38b8046f0a5f7359347f4b8469b3c2effa1ca
Cliff Curry <ccurry@google.com>
2014-07-14 20:24:03 -0700

    Initial Commit

---
+++ group-project/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  group-project
+//
+//  Created by Cliff Curry on 7/14/14.
+//  Copyright (c) 2014 Cliff Curry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
